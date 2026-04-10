9a8bb85dbfb6b4227540bfea10648aca4ebaad1a
Penguini128 <tommy.tpg03@gmail.com>
2026-04-10 10:51:12 -0400

    added imbal.classification.plot_roc

---
+++ development-tests/4-2026/4-10-26/memory_leak_test.py
@@ -0,0 +1,58 @@
+"""
+Import packages
+"""
+import imbal
+import os
+import numpy as np
+from PIL import Image
+from keras import layers, optimizers
+
+"""
+Load data
+"""
+SDO_DATA_PATH = '../../../tutorials/data/SDOBenchmark' # Ensure data is located at this path
+
+def load_sdo_data(data_path):
+    # Load labels (log peak flux)
+    with open(os.path.join(data_path, 'log_peak_flux.txt'), 'r') as file:
+        contents = file.read().strip()
+        loaded_data_fluxes = np.array([float(x) for x in contents.split('\n')])
+
+    # Load images (10 images per sample, 256x256 per image)
+    loaded_images = np.zeros((len(loaded_data_fluxes), 256, 256, 10), dtype=np.float32)
+    for i in range(len(loaded_data_fluxes)):
+        print(f'Loading SDO samples [{i+1}/{len(loaded_data_fluxes)}]', end='\r')
+        image_list = [Image.open(os.path.join(data_path, f'sdo_subset_sample_{i}_image_{x}.jpg')).convert('L') for x in range(10)]
+        stacked_images = np.stack(image_list, axis=-1) # Images stacked along channels
+        loaded_images[i] = stacked_images / 255.0 # Normalize black and white pixel values from 0 to 1
+
+    print(f'\n{len(loaded_data_fluxes)} data samples loaded successfully')
+    return loaded_images, loaded_data_fluxes
+
+# Load train and test data via function defined above
+x_train, y_train = load_sdo_data(os.path.join(SDO_DATA_PATH, 'training'))
+x_test, y_test = load_sdo_data(os.path.join(SDO_DATA_PATH, 'test'))
+
+print(
+    f'Loaded data with the following shapes:\n'
+    f'\tx_train: {x_train.shape}\n'
+    f'\ty_train: {y_train.shape}\n'
+    f'\tx_test: {x_test.shape}\n'
+    f'\ty_test {y_test.shape}'
+)
+
+dataset = imbal.regression.DatasetWithBatching(
+    x_train,
+    y_train,
+    num_batches=10
+)
+
+counter = 0
+while True:
+    counter += 1
+    print(counter)
+    dataset = imbal.regression.DatasetWithBatching(
+        x_train,
+        y_train,
+        num_batches=10
+    )
