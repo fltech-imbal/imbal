75abb744f9d71ec55e288319e45134f228e6151d
RocketBlaster05 <danielx1611@gmail.com>
2026-05-13 14:10:48 -0400

    gradcam documentation source files and tweaks

---
+++ development-tests/5-2026/5-8-2026-Dan/grad-cam-test.py
@@ -0,0 +1,166 @@
+"""
+Import packages
+"""
+import imbal
+import os
+import numpy as np
+from PIL import Image
+import tensorflow as tf
+import keras
+from keras import layers, optimizers, callbacks, metrics
+
+seed = 42
+tf.keras.utils.set_random_seed(
+    seed
+)
+
+"""
+Load data
+"""
+SDO_DATA_PATH = '../../../tutorials/data/SDOBenchmark' # Ensure data is located at this path
+
+def load_sdo_data(data_path):
+    # Load labels (log peak flux)
+    with open(os.path.join(data_path, 'log_peak_flux.txt'), 'r') as file:
+        contents = file.read().strip()
+        loaded_data_fluxes = np.array([float(x) for x in contents.split('\n')])
+
+    # Load images (10 images per sample, 256x256 per image)
+    loaded_images = np.zeros((len(loaded_data_fluxes), 128, 128, 1), dtype=np.float32)
+    for i in range(len(loaded_data_fluxes)):
+        print(f'Loading SDO samples [{i+1}/{len(loaded_data_fluxes)}]', end='\r')
+        image_list = Image.open(os.path.join(data_path, f'sdo_subset_sample_{i}.jpg')).convert('L')
+        stacked_images = np.array(image_list).reshape(128, 128, 1) # Images stacked along channels
+        loaded_images[i] = stacked_images / 255.0 # Normalize black and white pixel values from 0 to 1
+
+    print(f'\n{len(loaded_data_fluxes)} data samples loaded successfully')
+
+    return loaded_images, loaded_data_fluxes
+
+# Load train and test data via function defined above
+x_train, y_train = load_sdo_data(os.path.join(SDO_DATA_PATH, 'training'))
+x_test, y_test = load_sdo_data(os.path.join(SDO_DATA_PATH, 'test'))
+y_train = (y_train > -4).astype(np.int32)
+y_test = (y_test > -4).astype(np.int32)
+
+print(
+    f'Loaded data with the following shapes:\n'
+    f'\tx_train: {x_train.shape}\n'
+    f'\ty_train: {y_train.shape}\n'
+    f'\tx_test: {x_test.shape}\n'
+    f'\ty_test {y_test.shape}'
+)
+
+"""
+Build model
+"""
+def build_simple_cnn():
+    input_layer = layers.Input((128, 128, 1))
+    x = layers.Conv2D(8, 3, activation='relu', padding='same')(input_layer)
+    x = layers.Conv2D(8, 3, activation='relu', padding='same', strides=(2, 2))(x)
+    x = layers.Conv2D(16, 3, activation='relu', padding='same')(x)
+    x = layers.Conv2D(16, 3, activation='relu', padding='same', strides=(2, 2))(x)
+    x = layers.Conv2D(32, 3, activation='relu', padding='same')(x)
+    x = layers.Conv2D(32, 3, activation='relu', padding='same', strides=(2, 2))(x)
+    x = layers.Dense(32, activation='relu')(x)
+    x = layers.Flatten()(x)
+    output_layer = layers.Dense(1, activation='sigmoid')(x)
+
+    model = imbal.classification.Model(inputs=input_layer, outputs=output_layer)
+    model.summary()
+    return model
+
+model = build_simple_cnn()
+
+"""
+Compile and train model
+"""
+LEARNING_RATE = 5e-5
+EPOCHS = 20
+BATCH_SIZE = 256
+
+model.compile(
+    optimizer=optimizers.Adam(learning_rate=LEARNING_RATE),
+    loss='binary_crossentropy',
+    metrics=['accuracy', metrics.F1Score(threshold=0.5)],
+)
+
+model.balanced_fit(
+    x_train,
+    y_train.reshape(-1, 1),
+    # class_weight={0: 0.505, 1: 0.495},
+    # class_weight=[[0.9, 0.1,], [0.6, 0.4], [0.5, 0.5]], # Uncomment to use varying class weights
+    epochs=EPOCHS,
+    batch_size=BATCH_SIZE,
+    stratify_batches=True # Ensure all batches have a similar data distribution
+)
+
+model.evaluate(x_test, y_test.reshape(-1, 1))
+
+"""
+Data and results visualization
+"""
+KDE_BIN_COUNT=32
+
+test_rare_mask = y_test == 1
+test_frequent_mask = ~test_rare_mask
+print('Number of test samples with log10 flux < -4:', np.sum(test_frequent_mask))
+print('Number of test samples with log10 flux >= -4:', np.sum(test_rare_mask))
+
+# Predict on test data
+test_predictions = []
+for i in range(0, len(x_test), BATCH_SIZE):
+    batch = x_test[i:i+BATCH_SIZE]
+    test_predictions.append(model.predict(batch))
+test_predictions = np.concatenate(test_predictions, axis=0)
+test_predictions = test_predictions.reshape(-1, 1)
+y_test = y_test.reshape(-1, 1)
+
+# Calculate metrics
+hss = imbal.metrics.HeidkeSkillScore(threshold=0.5)
+hss.update_state(y_test, test_predictions)
+
+f1 = keras.metrics.F1Score(threshold=0.5)
+f1.update_state(y_test, test_predictions)
+
+print(
+    f'Heikde Skill Score: {hss.result()[0]:.4f}\n'
+    f'F1 Score: {f1.result()[0]:.4f}\n'
+)
+
+rounded_predictions = np.round(test_predictions).astype(np.int32).reshape(-1)
+y_test_flat = y_test.reshape(-1)
+
+true_positive_mask = (y_test_flat == 1) & (rounded_predictions == 1)
+false_positive_mask = (y_test_flat == 0) & (rounded_predictions == 1)
+
+true_positives = x_test[true_positive_mask]
+false_positives = x_test[false_positive_mask]
+
+imbal.classification.plot_confusion_matrix(
+    y_test,
+    test_predictions,
+    save_figure='classification-explanation-confusion-matrix.png'
+)
+
+if len(true_positives) > 0:
+    imbal.classification.gradcam_explain_image_sample(
+        sample=true_positives[0],
+        model=model,
+        class_names=["common", "rare"],
+        actual_label=1,
+        show=True,
+        save_figure=True,
+        figure_save_path='grad-cam-classification-true-positive-explanation.png'
+    )
+
+if len(false_positives) > 0:
+    imbal.classification.gradcam_explain_image_sample(
+        sample=false_positives[0],
+        model=model,
+        class_names=["common", "rare"],
+        actual_label=0,
+        show=True,
+        save_figure=True,
+        figure_save_path='grad-cam-classification-false-positive-explanation.png'
+    )
