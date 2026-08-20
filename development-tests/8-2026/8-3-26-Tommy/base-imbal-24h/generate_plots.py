b9426b016fa5f012c31bd9abc28d2ee8fc8486de
Tommy G <tommy.tpg03@gmail.com>
2026-08-20 16:13:22 -0400

    minor changes

--- development-tests/8-2026/8-3-26-Tommy/base-imbal-24h/generate_plots.py
+++ development-tests/8-2026/8-3-26-Tommy/base-imbal-24h/generate_plots.py
@@ -11,7 +11,7 @@ FULL_NAME = '_decoupled_w_validation_ae_3'
 USE_DELTA = False
 WEIGHT_CANDIDATES = False
 INCLUDE_IDS = True
-SAVE = False
+SAVE = True
 
 # def pcc(y_true, y_pred):
 #     y_true = tf.reshape(y_true, tf.shape(y_pred))
@@ -29,23 +29,24 @@ SAVE = False
 
 
 # Load the entire model (architecture, weights, and optimizer state)
-# model = tf.keras.models.load_model(
-#     f'models/{DATA_PREFIX}{FULL_NAME}.keras',
-#     custom_objects={
-#         # 'loss_fn': loss_fn,
-#         'Model' : imbal.regression.Model
-#     }
-# )
-# print(type(model))
-#
-# # Verify the model structure
-# model.summary()
+model = tf.keras.models.load_model(
+    f'models/{DATA_PREFIX}{FULL_NAME}.keras',
+    custom_objects={
+        # 'loss_fn': loss_fn,
+        'Model' : imbal.regression.Model
+    }
+)
+print(type(model))
+
+# Verify the model structure
+model.summary()
 
 
 def load_sep_ec(path_prefix):
     training_data = pd.read_csv(path_prefix + '_training.csv')
     test_data = pd.read_csv(path_prefix + '_test.csv')
     val_data = pd.read_csv(path_prefix + '_validation.csv')
+    columns = training_data.columns
     if USE_DELTA:
         training_labels = training_data.pop("delta_log_Intensity")
         val_labels = val_data.pop("delta_log_Intensity")
@@ -65,9 +66,9 @@ def load_sep_ec(path_prefix):
     training_labels = training_labels.to_numpy()
     val_labels = val_labels.to_numpy()
     test_labels = test_labels.to_numpy()
-    return (training_data, training_labels, training_ids), (val_data, val_labels, val_ids), (test_data, test_labels, test_ids)
+    return columns, (training_data, training_labels, training_ids), (val_data, val_labels, val_ids), (test_data, test_labels, test_ids)
 
-(x_train, y_train, train_ids), (x_val, y_val, val_ids), (x_test, y_test, test_ids) = load_sep_ec(
+columns, (x_train, y_train, train_ids), (x_val, y_val, val_ids), (x_test, y_test, test_ids) = load_sep_ec(
     f"{DATA_PATH}/{DATA_PREFIX}{'_w_ids' if INCLUDE_IDS else ''}",
 )
 
@@ -102,6 +103,18 @@ rare_mae = np.mean(np.abs(rare_predictions - rare_labels))
 print(model.best_weight_index)
 
 import matplotlib.pyplot as plt
+
+dates_map = {
+    10 : "01-22-2012",
+    22 : "08-31-2012",
+    27 : "05-21-2013",
+    30 : "01-06-2014",
+    32 : "02-19-2014",
+    36 : "11-01-2014",
+    41 : "12-31-2015",
+    43 : "09-04-2017"
+}
+
 def plot_true_vs_predictions(
     labels,
     predictions,
@@ -127,7 +140,7 @@ def plot_true_vs_predictions(
         plt.scatter(labels, predictions, color="#00FF0044" if color is None else color, s=size, marker=marker)
     else:
         for value in ids.unique():
-            plt.scatter(labels[ids == value], predictions[ids == value], s=size, marker=marker, label=value)
+            plt.scatter(labels[ids == value], predictions[ids == value], s=size, marker=marker, label=dates_map[value])
     plt.xlabel(x_axis_label)
     plt.ylabel(y_axis_label)
     plt.xlabel("True Label" if x_axis_label is None else x_axis_label)
@@ -136,33 +149,47 @@ def plot_true_vs_predictions(
     plt.ylim(data_min, data_max)
     if title is not None:
         plt.title(title)
-    if save_figure is not None:
-        plt.savefig(save_figure)
-    # Save plot if path is specified
-    if save_figure is not None:
-        plt.savefig(save_figure)
     plt.legend()
+    if save_figure is not None:
+        plt.savefig(save_figure)
+
     plt.show()
 
 plot_true_vs_predictions(
     y_test,
     predictions,
     title=f'SEP-E - Common MAE: {common_mae:.4f}, Rare MAE: {rare_mae:.4f}, AORE: {(mae + rare_mae)/2:.4f}{f", Alpha: {[0.1*(i+1) for i in range(10)][model.best_weight_index]:.1f}" if WEIGHT_CANDIDATES else ""}',
-    save_figure=f"{OUTPUT_PATH}/{DATA_PREFIX}{FULL_NAME}.png" if SAVE else None,
+    save_figure=f"{OUTPUT_PATH}/{DATA_PREFIX}{FULL_NAME}_multicolor.png" if SAVE else None,
     ids=test_ids if INCLUDE_IDS else None,
     size=3 if INCLUDE_IDS else None,
 )
 
-# imbal.regression.tsne_visualization(
-#     model,
-#     x_test,
-#     y_test,
-#     # perplexity=300,
-#     save_figure='sep-proton-time-series-tsne.png'
-# )
+imbal.regression.tsne_visualization(
+    model,
+    x_test,
+    y_test,
+    # perplexity=300,
+    save_figure=f'{OUTPUT_PATH}/{DATA_PREFIX}{FULL_NAME}-sep-proton-time-series-tsne.png'
+)
 
-# columns = columns.tolist()
-#
+columns = columns.tolist()
+
+rare_samples_mispredicted = x_test[(y_test > np.log(10)) & (predictions < np.log(10))]
+rare_label_mispredicted = y_test[(y_test > np.log(10)) & (predictions < np.log(10))]
+rare_predictions_mispredicted = predictions[(y_test > np.log(10)) & (predictions < np.log(10))]
+
+misprediction_errors = np.abs(rare_predictions_mispredicted - rare_label_mispredicted)
+error_indices = np.argsort(misprediction_errors)
+
+high_error_rare_sample = rare_samples_mispredicted[error_indices[-1]]
+
+imbal.regression.shap_explain_tabular_sample(
+    sample=high_error_rare_sample,
+    model=model,
+    training_data=x_train,
+    feature_names=columns,
+)
+
 # imbal.regression.lime_explain_tabular_sample(
 #     x_test[common_sample_mask][0],
 #     model,
